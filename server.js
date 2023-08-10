289e352e20b6d12062f0655100e066b9da536a8a
Jabril-Jeylani <jabrilmj@gmail.com>
2023-08-10 15:09:36 -0600

    index.jsx view rendered

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const pokemon = require('./models/pokemon')
+
+const app = express()
+
+const PORT = 3000
+
+const jsxEngine = require('jsx-view-engine')
+app.set('view engine', 'jsx')
+app.engine('jsx', jsxEngine())
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Pokemon App!')
+})
+
+app.get('/pokemon', (req, res) => {
+    res.render('Index')
+})
+
+app.listen(PORT, () => {
+    console.log('Listening on port: ' + PORT)
+})
